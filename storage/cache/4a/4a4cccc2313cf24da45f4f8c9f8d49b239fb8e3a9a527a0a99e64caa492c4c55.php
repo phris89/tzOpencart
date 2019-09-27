9949c56de543a41a1ceb82c8ef0527c9428b3b09
phris89 <petr0789@yandex.ru>
2019-09-27 20:08:47 +0300

    origin installation

---
+++ storage/cache/4a/4a4cccc2313cf24da45f4f8c9f8d49b239fb8e3a9a527a0a99e64caa492c4c55.php
@@ -0,0 +1,49 @@
+<?php
+
+/* default/template/common/content_top.twig */
+class __TwigTemplate_41332949cdbf0ec0889cc217ae7013ee6b4d2423e2ee0210de275727d4a221d4 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["modules"]) ? $context["modules"] : null));
+        foreach ($context['_seq'] as $context["_key"] => $context["module"]) {
+            // line 2
+            echo $context["module"];
+            echo "
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['module'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+    }
+
+    public function getTemplateName()
+    {
+        return "default/template/common/content_top.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  23 => 2,  19 => 1,);
+    }
+}
+/* {% for module in modules %}*/
+/* {{ module }}*/
+/* {% endfor %}*/
